755920c29b229282bf5a11873a75335f1221fba0
Alla Trukhan <allatrukhan1993@gmail.com>
2023-09-04 11:21:12 +0300

    modified:   src/store/searchSlice.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/CurrentLocationWeather/CurrentLocationWeather.js 	modified:   src/components/SearchCityWeather/SearchCityWeather.js 	modified:   src/components/WeatherCard/WeatherCard.js 	modified:   src/components/WeatherGalleryCards/WeatherGalleryCards.js 	modified:   src/components/index.js 	modified:   src/services/weather.service.js 	deleted:    src/store/currentLocation.slice.js 	modified:   src/store/weatherSlice.js 	src/components/CurrentLocationWeather/CurrentLocationWeather.css 	src/components/SearchCityWeather/SearchCityWeather.css 	src/components/WeatherCard/WeatherCard.css 	src/components/WeatherGalleryCards/WeatherGalleryCards.css 	src/pexels-johannes-plenio-1118873.jpg

--- src/store/searchSlice.js
+++ src/store/searchSlice.js
@@ -1,11 +1,22 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {weatherService} from "../services/weather.service";
-// import searchValue from "../components/SearchCityWeather/SearchCityWeather";
 
 const initialState = {
     searchObject: [],
     status: null
 }
+
+export const refreshCard = createAsyncThunk(
+    'refreshCard/refreshCard',
+    async (name) => {
+        try {            
+            return await weatherService.getWeatherBySearch(name)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+)
+
 
 export const searchGetCityWeather = createAsyncThunk(
     'searchCityWeather/searchGetCityWeather',
@@ -20,18 +31,74 @@ export const searchGetCityWeather = createAsyncThunk(
         }
     }
 )
+
+export const locationGetCityWeather = createAsyncThunk(
+    'locationCityWeather/locationGetCityWeather',
+    async (obj) => {
+        if (!obj.lat&&!obj.long){
+            return null
+        }
+
+    try{
+            return await weatherService.getWeatherByLocation(obj.lat, obj.long)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+)
+
 const searchCityWeatherSlice = createSlice({
     name: 'searchCityWeather',
     initialState,
-    reducers: {},
+    reducers: {
+        deleteCard: (state, action)=>{ 
+            state.searchObject = state.searchObject.filter(weatherData => weatherData.id !== action.payload.id)
+            },
+    },
     extraReducers: {
         [searchGetCityWeather.pending]: (state) => {
             state.status = 'pending'
         },
         [searchGetCityWeather.fulfilled]: (state, action) => {
-            
+            state.status = 'fulfilled'
             try {
-                state.searchObject = action.payload.data
+                console.log(action.payload.data.name);
+                const indx = state.searchObject.findIndex(el=> el.name===action.payload.data.name);
+                if (indx===-1){
+                    state.searchObject.push(action.payload.data);
+                }
+                // state.searchObject.forEach((el) => {
+                //     if (el.name === action.payload.data.name){return}
+                // })
+                // state.searchObject.push(action.payload.data)
+                // state.searchObject = action.payload.data;
+                // state.searchObject.push(action.payload.data);
+                    // state.searchObject = {...action.payload.data, id:new Date().getTime()}
+            } catch (e) {
+                state.searchObject = []
+            }
+        },
+        [refreshCard.pending]: (state) => {
+            state.status = 'pending'
+        },
+        [refreshCard.fulfilled]: (state, action) => {
+            state.status = 'fulfilled'
+            try {
+                const indx = state.searchObject.findIndex(el=> el.name===action.payload.data.name);
+                state.searchObject.splice(indx, 1, action.payload.data)
+            } catch (e) {
+                state.searchObject = []
+            }
+        },
+
+        [locationGetCityWeather.pending]: (state) => {
+            state.status = 'pending'
+        },
+        [locationGetCityWeather.fulfilled]: (state, action) => {
+            state.status = 'fulfilled'
+            try {
+                state.searchObject.push(action.payload.data);
+                // state.searchObject = action.payload.data
                     // state.searchObject = {...action.payload.data, id:new Date().getTime()}
             } catch (e) {
                 state.searchObject = []
@@ -42,4 +109,8 @@ const searchCityWeatherSlice = createSlice({
 });
 
 const searchCityWeatherReduser = searchCityWeatherSlice.reducer;
+export const {deleteCard} = searchCityWeatherSlice.actions;
 export default searchCityWeatherReduser;
+
+
+
